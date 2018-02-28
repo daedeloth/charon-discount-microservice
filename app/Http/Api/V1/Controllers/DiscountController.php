a10be29c097e383aae41235e589abd90cd5fe4c1
Thijs Van der Schaeghe <thijs@catlab.be>
2018-02-28 19:28:44 +0100

    Initial commit: laravel with unrequired stuff removed.

---
+++ app/Http/Api/V1/Controllers/DiscountController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Api\V1\Controllers;
+
+/**
+ * Class DiscountController
+ * @package App\Http\Api\V1\Controllers
+ */
+class DiscountController
+{
+
+}
---
+++ app/Http/Api/V1/ResourceDefinitions/DiscountResourceDefinition.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Api\V1\ResourceDefinitions;
+
+/**
+ * Class DiscountResourceDefinition
+ * @package App\Http\Api\V1\ResourceDefinitions
+ */
+class DiscountResourceDefinition
+{
+
+}
